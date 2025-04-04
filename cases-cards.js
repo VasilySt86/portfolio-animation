0342ea8d7aec36352402f2f38c56fe53ff4f5935
VasilySt86 <108338835+VasilySt86@users.noreply.github.com>
2025-04-04 20:24:20 +0200

    Update cases-cards.js

--- cases-cards.js
+++ cases-cards.js
@@ -1,18 +1,1 @@
 console.log("Скрипт cases-cards.js работает!");
-document.body.innerHTML = `
-  <div style="
-    position:fixed;
-    top:0;left:0;
-    width:100vw;
-    height:100vh;
-    background:white;
-    z-index:9999;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    font-size:24px;
-  ">
-    Сайт временно недоступен.  
-    <br>Обратитесь к разработчику.
-  </div>
-`;
